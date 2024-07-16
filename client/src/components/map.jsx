e9f33112012283bd7271c425fbb46b24091daae6
Matias Rómoli <122396770+matias-romoli@users.noreply.github.com>
2024-07-16 15:36:10 -0300

    Update map.jsx

--- client/src/components/map.jsx
+++ client/src/components/map.jsx
@@ -11,8 +11,7 @@ function Map() {
       {url.map((url) => (
         <div key={url.id} className="http">
           <a className="url__text">
-            {import.meta.env.VITE_URL}
-            {url.short}
+            "https://short-url-3l4s.onrender.com/" + {url.short}
           </a>
           <button className="url__btn" onClick={() => copyClipboard(url.short)}>
             {!copied ? (
